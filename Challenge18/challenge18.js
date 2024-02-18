8cf9b2b554fbad771b3a85d3429d4791bc2f10d6
Kingcrusher <tiwarivikrant9764@gmail.com>
2024-02-18 13:43:35 +0530

    "Challenge 18 commit"

---
+++ Challenge18/challenge18.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const port = 3000
+
+mongoose.connect("mongodb://127.0.0.1:27017/mydatabase")
+const db = mongoose.connection
+db.on("error", (err)=>{
+    console.log("MongoDB Connection Error: ", err)
+})
+db.once("open", ()=>{
+    console.log("Connected to MongoDB")
+})
+
+const UserSchema = new mongoose.Schema({
+    Username: String,
+    Email: String,
+})
+const User = mongoose.model("Users", UserSchema);
+
+app.get("/users", async (req, res)=>{
+    const userData = await User.find();
+    res.json({UserData: userData});
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started at https://localhost:${port}`)
+})
